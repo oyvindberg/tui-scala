41820b1e1f675d9977f0a49861d148a4892fe80e
Øyvind Raddum Berg <elacin@gmail.com>
2022-12-02 09:07:49 +0100

    port sparkline

--- demo/shared/src/scala/tui/demo.scala
+++ demo/shared/src/scala/tui/demo.scala
@@ -1,68 +1,20 @@
 package tui
 
-import tui.crossterm._
-
-import java.util.Optional
-
 object demo {
-  def main(args: Array[String]): Unit = {
-    val jni = new CrosstermJni();
-    println("Hola")
-    println(jni.terminalSize())
-    println(jni.poll(new Duration(secs = 1, nanos = 0)))
-    jni.enqueueTerminalClear(ClearType.All)
-    val opt = Optional.of[Color](new Color.Rgb(1, 2, 3))
-    jni.enqueueStyleSetColors(opt, Optional.empty());
-    jni.enqueueStyleSetAttributes(new Attributes(java.util.List.of(Attribute.Bold, Attribute.Dim)));
-    jni.enqueueEventEnableFocusChange()
-    jni.enqueueEventEnableMouseCapture()
-    jni.enqueueEventEnableBracketedPaste()
-    jni.enqueueEventPushKeyboardEnhancementFlags(new KeyboardEnhancementFlags(KeyboardEnhancementFlags.REPORT_EVENT_TYPES))
-    println(new Color.Rgb(1, 2, 3))
-    jni.enableRawMode()
+  val Demos = Map[String, Array[String] => Unit](
+    "sparkline" -> tui.examples.sparkline.Main.main,
+    "barchart" -> tui.examples.barchart.Main.main
+  )
 
-    var continue = true
-    while (continue) {
-      val e = jni.read()
-      jni.enqueueCursorMoveToColumn(0)
-      jni.enqueueStylePrint("event: " + e + "\n")
-      jni.flush()
-      e match {
-        case x: Event.Key =>
-          x.keyEvent.code match {
-            case c: KeyCode.Char if c.c() == 'q' => continue = false
-            case _                               => ()
-          }
-        case _ => ()
-      }
-    }
-    var i = 0
-    while (i < 20) {
-      val color = if (i % 2 == 0) new crossterm.Color.Red() else new crossterm.Color.Yellow()
-      jni.enqueueCursorMoveToColumn(0)
-      jni.enqueueStyleSetColors(Optional.of(color), Optional.empty())
-      jni.enqueueStylePrint("flaff: " + i + "\n")
-      jni.enqueueCursorMoveLeft(1)
-      jni.flush()
-      //      Thread.sleep(50L)
-      i += 1
-    }
-    jni.enqueueStyleResetColor()
-//    jni.enqueueTerminalClear(ClearType.Purge)
-//    jni.enqueueTerminalClear(ClearType.All)
-    jni.enqueueStylePrint("done: " + i + "\n")
-    jni.disableRawMode()
-    jni.enqueueEventDisableFocusChange()
-    jni.enqueueEventDisableMouseCapture()
-    jni.enqueueEventDisableBracketedPaste()
-    jni.enqueueEventPopKeyboardEnhancementFlags()
-    jni.flush()
+  def main(args: Array[String]): Unit =
+    args.headOption match {
+      case Some(name) =>
+        Demos.get(name) match {
+          case Some(demo) => demo(args.drop(1))
+          case None =>
+            System.err.println(s"$name is not among ${Demos.keys.mkString(",")}")
 
-//    jni.enqueueStyleSetStyle(
-//      Optional.of(new Color.AnsiValue(1)),
-//      Optional.of(new Color.AnsiValue(81)),
-//      Optional.empty,
-//      new Attributes(java.util.List.of(Attribute.Bold, Attribute.CrossedOut))
-//    )
-  }
+        }
+      case None => System.err.println(s"specify which demo one of ${Demos.keys.mkString(",")} as parameter")
+    }
 }
