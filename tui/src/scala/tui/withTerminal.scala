a9192d0b90834eb65c679814037a9b2ffd1f1d30
Øyvind Raddum Berg <elacin@gmail.com>
2022-12-09 15:00:55 +0100

    Add `withTerminal` which uses `try/finally` to always restore terminal

---
+++ tui/src/scala/tui/withTerminal.scala
@@ -0,0 +1,26 @@
+package tui
+
+import tui.backend.CrosstermBackend
+import tui.crossterm.{Command, CrosstermJni}
+import tui.terminal.Terminal
+
+object withTerminal {
+  def apply[T](f: (CrosstermJni, Terminal) => T): T = {
+    val jni = new CrosstermJni
+    // setup terminal
+    jni.enableRawMode()
+    jni.execute(new Command.EnterAlternateScreen(), new Command.EnableMouseCapture())
+
+    val backend = new CrosstermBackend(jni)
+
+    val terminal = Terminal.init(backend)
+
+    try f(jni, terminal)
+    finally {
+      // restore terminal
+      jni.disableRawMode()
+      jni.execute(new Command.LeaveAlternateScreen(), new Command.DisableMouseCapture())
+      backend.show_cursor()
+    }
+  }
+}
