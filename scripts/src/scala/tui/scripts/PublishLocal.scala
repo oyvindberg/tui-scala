3f367bfe29403813bc7a959928e53faa5e82eb7d
Øyvind Raddum Berg <elacin@gmail.com>
2022-12-23 23:34:37 +0100

    Update to bleep 0.0.1-M23

--- scripts/src/scala/tui/scripts/PublishLocal.scala
+++ scripts/src/scala/tui/scripts/PublishLocal.scala
@@ -6,7 +6,7 @@ import bleep.plugin.dynver.DynVerPlugin
 object PublishLocal extends BleepScript("PublishLocal") {
   def run(started: Started, commands: Commands, args: List[String]): Unit = {
     val dynVer = new DynVerPlugin(baseDirectory = started.buildPaths.buildDir.toFile, dynverSonatypeSnapshots = true)
-    val projects = started.build.explodedProjects.keys.toList.filter(projectsToPublish)
+    val projects = started.build.explodedProjects.keys.toArray.filter(projectsToPublish)
 
     commands.publishLocal(
       bleep.commands.PublishLocal.Options(
