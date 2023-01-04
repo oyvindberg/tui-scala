c95a9d7d2f0aea75dd6ad1fd458092be8fa7c243
Øyvind Raddum Berg <elacin@gmail.com>
2023-01-04 01:47:37 +0100

    Apple m1 continue (#30)
    - bump to bleep 0.0.1-M24
    - use bleep-build/bleep-setup-action@0.0.1
    - fix broken platform detection code in jni plugin (uname -sm somehow picks up x86_64 from an arm process. I'm baffled)

--- scripts/src/scala/tui/scripts/GenJniLibrary.scala
+++ scripts/src/scala/tui/scripts/GenJniLibrary.scala
@@ -13,10 +13,18 @@ object GenJniLibrary extends BleepScript("GenJniLibrary") {
       nativeBuildTool = new Cargo(release = true),
       libName = "crossterm",
       env = sys.env.toList
-    )
+    ) {
+      override lazy val nativePlatform: String =
+        OsArch.current match {
+          case OsArch.LinuxAmd64    => "x86_64-linux"
+          case OsArch.WindowsAmd64  => "x86_64-msys_nt-10.0-20348"
+          case OsArch.MacosAmd64    => "x86_64-darwin"
+          case OsArch.MacosArm64(_) => "arm64-darwin"
+          case other: OsArch.Other  => sys.error(s"not implemented: $other")
+        }
+    }
 
   override def run(started: Started, commands: Commands, args: List[String]): Unit = {
-
     val jniNative = crosstermJniNativeLib(started)
     val jniPackage = new JniPackage(started.buildPaths.buildDir, jniNative)
 
